a4232d68985b03ef980ead6e23d7424533172f03
SeungUkShin <krdut1@gmail.com>
2022-08-07 18:21:48 +0900

    [신승욱] init

--- FingerOfTheDay/ViewController.swift
+++ FingerOfTheDay/ViewController.swift
@@ -5,13 +5,19 @@
 //  Created by ukBook on 2022/08/07.
 //
 
+// 롱프레스 및 햅틱진동 구현하기
+// https://h1guitar.tistory.com/228
+
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("\(#function)")
     }
 
 

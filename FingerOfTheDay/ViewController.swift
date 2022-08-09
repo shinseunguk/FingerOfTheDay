1f265317a28d3b32f1c0e63db07ce7ecc0172942
Wetax_iOS <wetaxmobile@gmail.com>
2022-08-09 18:00:03 +0900

    [신승욱] slider 추가

--- FingerOfTheDay/ViewController.swift
+++ FingerOfTheDay/ViewController.swift
@@ -8,25 +8,57 @@
 // 롱프레스 및 햅틱진동 구현하기
 // https://h1guitar.tistory.com/228
 
+// tab and touch example
+// https://moonibot.tistory.com/50
+
+// TGP Control
+// https://github.com/SwiftArchitect/TGPControls
+
 import UIKit
 import AudioToolbox
+import TGPControls
 
 class ViewController: UIViewController {
     
     let helper : Helper = Helper()
     
+    // 터치 할수있는 수
+    var fingerCount : Int = 2;
+    
     @IBOutlet weak var selectServerButton: UIButton!
+    @IBOutlet var uiView: UIView!
+    
+    @IBOutlet weak var oneTo10Labels: TGPCamelLabels!
+    @IBOutlet weak var oneTo10Slider: TGPDiscreteSlider!
+    
+    private func localizedStrings(_ key: String) -> [String] {
+        return NSLocalizedString(key, comment: "")
+            .split(separator: " ")
+            .map({ (substring) -> String in
+                return "\(substring)"
+            })
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("\(#function)")
+        oneTo10Labels.names = localizedStrings("oneTo10Labels.numbers")
+        oneTo10Slider.ticksListener = oneTo10Labels
         
+//        print("\(#function)")
+//
         let longPress = UILongPressGestureRecognizer(target: self, action: #selector(longPress(_:)))
-             self.selectServerButton.addGestureRecognizer(longPress)
-            // 롱프레스 시간설정
-            longPress.minimumPressDuration = 3
+//             self.selectServerButton.addGestureRecognizer(longPress)
+        self.uiView.addGestureRecognizer(longPress)
+//            // 롱프레스 시간설정
+        longPress.minimumPressDuration = 3
+        
+        oneTo10Slider.addTarget(self, action: #selector(ViewController.valueChanged(_:event:)), for: .valueChanged)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.uiView.backgroundColor = UIColor(patternImage: UIImage(named: "space.jpeg")!)
     }
     
     @objc func longPress(_ guesture: UILongPressGestureRecognizer) {
@@ -37,10 +69,38 @@ class ViewController: UIViewController {
                 // 구현할 기능 넣기
             }
                 return
-      }
+    }
+    
+    // 터치가 시작될 때 호출
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // 현재 발생한 터치 이벤트를 가지고 옴
+        let touchCount = touches.count
+        let touch = touches.first! as UITouch
+        let tapCount = touch.tapCount
+        
+        print("\(touchCount) touches")
+        print("\(tapCount) taps")
+    }
 
-    @IBAction func tapGesture(_ sender: Any) {
-        print("dd")
+    // 터치된 손가락이 움직였을 때 호출
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        let touch = touches.first! as UITouch
+//
+//        print("Touches Moved ", String(touches.count))
+//    }
+
+    // 화면에서 손가락을 떼엇을 때 호출
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        let touch = touches.first! as UITouch
+//
+//        print("Touches Ended tapCount", String(touch.tapCount))
+//        print("Touches Ended ", String(touches.count))
+//    }
+    
+    @objc func valueChanged(_ sender: TGPDiscreteSlider, event:UIEvent) {
+        print(Int(sender.value)+2)
+        
+        fingerCount = Int(sender.value)+2
     }
     
 }
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FingerOfTheDay' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FingerOfTheDay
+  pod 'TGPControls'
+
+end

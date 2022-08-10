50bb91df08c1c4089b82816f9efc5e590a0cc1e3
Wetax_iOS <wetaxmobile@gmail.com>
2022-08-10 14:55:44 +0900

    [신승욱] fadein, fadeout extension 추가

--- FingerOfTheDay/Helper/Helper.swift
+++ FingerOfTheDay/Helper/Helper.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import UIKit
+import TGPControls
 
 class Helper : UIViewController {
     func showAlertAction1(vc: UIViewController?, preferredStyle: UIAlertController.Style = .alert, title: String = "", message: String = "", completeTitle: String = "확인", _ completeHandler:(() -> Void)? = nil){
@@ -28,4 +29,79 @@ class Helper : UIViewController {
                     currentVc.present(alert, animated: true, completion: nil)
                 }
     }
+}
+
+extension UILabel {
+
+    func fadeIn(_ duration: TimeInterval? = 0.3, onCompletion: (() -> Void)? = nil) {
+        self.alpha = 0
+        self.isHidden = false
+        UIView.animate(withDuration: duration!,
+                       animations: { self.alpha = 1 },
+                       completion: { (value: Bool) in
+                          if let complete = onCompletion { complete() }
+                       }
+        )
+    }
+
+    func fadeOut(_ duration: TimeInterval? = 0.3, onCompletion: (() -> Void)? = nil) {
+        UIView.animate(withDuration: duration!,
+                       animations: { self.alpha = 0 },
+                       completion: { (value: Bool) in
+                           self.isHidden = true
+                           if let complete = onCompletion { complete() }
+                       }
+        )
+    }
+
+}
+
+extension TGPCamelLabels {
+
+    func fadeIn(_ duration: TimeInterval? = 0.3, onCompletion: (() -> Void)? = nil) {
+        self.alpha = 0
+        self.isHidden = false
+        UIView.animate(withDuration: duration!,
+                       animations: { self.alpha = 1 },
+                       completion: { (value: Bool) in
+                          if let complete = onCompletion { complete() }
+                       }
+        )
+    }
+
+    func fadeOut(_ duration: TimeInterval? = 0.3, onCompletion: (() -> Void)? = nil) {
+        UIView.animate(withDuration: duration!,
+                       animations: { self.alpha = 0 },
+                       completion: { (value: Bool) in
+                           self.isHidden = true
+                           if let complete = onCompletion { complete() }
+                       }
+        )
+    }
+
+}
+
+extension TGPDiscreteSlider {
+
+    func fadeIn(_ duration: TimeInterval? = 0.3, onCompletion: (() -> Void)? = nil) {
+        self.alpha = 0
+        self.isHidden = false
+        UIView.animate(withDuration: duration!,
+                       animations: { self.alpha = 1 },
+                       completion: { (value: Bool) in
+                          if let complete = onCompletion { complete() }
+                       }
+        )
+    }
+
+    func fadeOut(_ duration: TimeInterval? = 0.5, onCompletion: (() -> Void)? = nil) {
+        UIView.animate(withDuration: duration!,
+                       animations: { self.alpha = 0 },
+                       completion: { (value: Bool) in
+                           self.isHidden = true
+                           if let complete = onCompletion { complete() }
+                       }
+        )
+    }
+
 }
